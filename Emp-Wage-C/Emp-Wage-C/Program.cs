b99ff8aa4bd31d6e4ce13a54b0d4923d5ac26a86
unknown <nilimawadal1@gmail.com>
2021-04-15 16:49:07 +0530

    [Nilima] Added Welcome Message

---
+++ Emp-Wage-C/Emp-Wage-C/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Emp_Wage_C
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WelCome To The Employee Wage");
+        }
+    }
+}
