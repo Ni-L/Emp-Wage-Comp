513c389015e5e8d4d6b53be524a1e3077f7a01c4
unknown <nilimawadal1@gmail.com>
2021-04-15 21:18:21 +0530

    [Nilima] Added Multiple Company Employee Wage

---
+++ Emp-Wage-C/Emp-Wage-C/CompanyEmpWage.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emp_Wage_C
+{
+    class CompanyEmpWage
+    {
+        /// <summary>
+        /// Creating Comapny Wage
+        /// </summary>
+        public string company;
+		public int empRatePerHour;
+		public int numOfWorkingDays;
+		public int maxHoursPerMonth;
+		public int totalEmpWage;
+
+		public CompanyEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
+		{
+			this.company = company;
+			this.empRatePerHour = empRatePerHour;
+			this.numOfWorkingDays = numOfWorkingDays;
+			this.maxHoursPerMonth = maxHoursPerMonth;
+		}
+
+		public void setTotalEmpWage(int totalEmpWage)
+		{
+			this.totalEmpWage = totalEmpWage;
+		}
+
+		public string toString()
+		{
+			return "total EmpWage for company :" + this.company + " is :" + this.totalEmpWage;
+		}
+	}
+}
+    
+
