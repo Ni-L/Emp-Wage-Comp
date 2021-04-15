59ea43b0c68851c9a4cc5b5c335ce8832c439b6d
unknown <nilimawadal1@gmail.com>
2021-04-15 16:58:08 +0530

    [Nilima] Added EmployeePresentAbsent

--- Emp-Wage-C/Emp-Wage-C/Program.cs
+++ Emp-Wage-C/Emp-Wage-C/Program.cs
@@ -7,6 +7,20 @@ namespace Emp_Wage_C
         static void Main(string[] args)
         {
             Console.WriteLine("WelCome To The Employee Wage");
+            int Present = 1;
+            Random random = new Random();
+            int EmpCheck = random.Next(0, 2);
+            Console.WriteLine("EmpCheck: " + EmpCheck);
+
+            if (EmpCheck == Present)
+            {
+                Console.WriteLine("Employee PRESENT");
+            }
+            else
+            {
+                Console.WriteLine("Employee ABSENT");
+            }
+            Console.Read();
         }
     }
 }
