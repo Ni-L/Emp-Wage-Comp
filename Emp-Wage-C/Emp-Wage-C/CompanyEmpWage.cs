7b746b4d986edee3e2419ef6051b73fe80d3fc1a
unknown <nilimawadal1@gmail.com>
2021-04-16 17:29:32 +0530

    [Nilima] Added Interface

---
+++ Emp-Wage-C/Emp-Wage-C/CompanyEmpWage.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpWageComputation
+{
+    class CompanyEmpWage
+    {
+		public string company;
+		public int empRatePerHour;
+		public int numOfWorkingDays;
+		public int maxHoursPerMonth;
+		public int totalEmpWage;
+
+		public CompanyEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth) //parameterized constructer
+		{
+			this.company = company;
+			this.empRatePerHour = empRatePerHour;
+			this.numOfWorkingDays = numOfWorkingDays;
+			this.maxHoursPerMonth = maxHoursPerMonth;
+		}
+
+		public void setTotalEmpWage(int totalEmpWage) //set method
+		{
+			this.totalEmpWage = totalEmpWage;
+		}
+
+		public string toString() //instance method 
+		{
+			return "total EmpWage for company :" + this.company + " is :" + this.totalEmpWage;
+		}
+	}
+}
+
